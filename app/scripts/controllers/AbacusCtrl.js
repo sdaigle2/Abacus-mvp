182e4b07f2383540476f65d72e046644a5087241
Brian Zhou <brian@intelliwheels.net>
2015-02-20 12:47:20 -0600

    progress bar clickable

--- app/scripts/controllers/AbacusCtrl.js
+++ app/scripts/controllers/AbacusCtrl.js
@@ -9,5 +9,23 @@
  */
 angular.module('abacuApp')
   .controller('AbacusCtrl', function ($scope) {
-      
+    $scope.progs=progesssec;
+
+
   });
+
+
+var progesssec={
+  1:false,
+  2:false,
+  3:false,
+  4:false,
+  5:false,
+  6:false,
+  7:false,
+  8:false,
+  9:false,
+  10:false,
+  11:false,
+  12:false
+};
