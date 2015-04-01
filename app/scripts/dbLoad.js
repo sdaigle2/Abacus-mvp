3eae195e3ef925f9b9dc13f4fe9b4bae8c97b43c
blackarctic <blackarctic@icloud.com>
2015-04-01 10:42:30 -0500

    moved cartData out of CartCtrl.js and into data section

--- app/scripts/dbLoad.js
+++ app/scripts/dbLoad.js
@@ -6,11 +6,23 @@
   This file loads the json database from app/data.
   It should be replaced with loading the actual data from the actual database as we move forward.
 */
-var frameDataFromDB;
 
+//get frame data
+var frameDataFromDB;
 $.getJSON('data/frameData.json')
   .done(function( json ) {
     frameDataFromDB = json;
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ', ' + error;
+    console.log( 'Request Failed: ' + err );
+  });
+
+//get cart data
+var cartDataFromDB;
+$.getJSON('data/cartData.json')
+  .done(function( json ) {
+    cartDataFromDB = json;
   })
   .fail(function( jqxhr, textStatus, error ) {
     var err = textStatus + ', ' + error;
