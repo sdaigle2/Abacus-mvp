3678b806eb3b7af89e455ed10944ac06fce204fc
Scott Blessing <scott.blessing@hotmail.com>
2015-04-07 15:24:12 -0500

    Set up Frames page
    Experience the joys of bootstrap while attempting to browse for frames

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -9,9 +9,54 @@
  */
 angular.module('abacuApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.frames = dummyFramesData; //TODO: You know the drill here - replace with actual data later
+
+    $scope.selectFrame = function (frameID) {
+      //Send user to abacus with chosen frame
+      alert("Not implemented - selectFrame("+frameID+")");
+    };
+
   });
+
+
+
+
+var dummyFramesData = [
+  {
+    frameID: 1,
+    manufacturer: "Ti Arrow",
+    name: "CKF",
+    desc: "Ti Arrow's Standard Rigid Frame Wheelchair made from Titanium",
+    basePrice: 2995,
+    baseWeight: 45,
+    imageURL: "images/d_panel_2.png"
+  },
+  {
+    frameID: 2,
+    manufacturer: "Intelliwheels",
+    name: "Intelliwheelchair",
+    desc: "Better than anything our \"competition\" makes",
+    basePrice: 1999,
+    baseWeight: 30,    
+    imageURL: "images/d_panel_1.png"
+  },
+  {
+    frameID: 3,
+    manufacturer: "Sony",
+    name: "Playstation 4",
+    desc: "This isn\'t a wheelchair, but you should still buy it",
+    basePrice: 499,
+    baseWeight: 10,
+    imageURL: "images/pangding.jpg"
+  },
+  {
+    frameID: 4,
+    manufacturer: "Intelliwheels",
+    name: "Pogo Stick",
+    desc: "A fun toy for children",
+    basePrice: 20,
+    baseWeight: 3,
+    imageURL: "images/d_panel_4.png"
+  }
+];
