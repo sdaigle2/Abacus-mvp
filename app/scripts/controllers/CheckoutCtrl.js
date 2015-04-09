a0fecfd46603cdb6b77f947a81d992cec6cc550f
Scott Blessing <scott.blessing@hotmail.com>
2015-04-09 16:00:41 -0500

    Checkout JS and some CSS
    Created function shells for Checkout
    "Finalized" checkout CSS

--- app/scripts/controllers/CheckoutCtrl.js
+++ app/scripts/controllers/CheckoutCtrl.js
@@ -12,6 +12,45 @@
 angular.module('abacuApp')
   .controller('CheckoutCtrl', function ($scope, $location, sharedVars) {
 
-    $scope.lulz = {};
+    //Login Form Model
+    $scope.loginForm = {
+      email: "",
+      pass: ""
+    };
+
+    //Return the user to their cart
+    $scope.returnToCart = function () {
+      $location.path('/cart');
+    };
+
+    //Validate user login, and then log them in
+    $scope.loginAsUser = function () {
+      //TODO: Validate $scope.loginForm inputs
+      //      Log user in
+      //      Send to "Info" Page
+
+      //loginForm.email should be verified as an email address by the HTML - it should be null if not an email
+
+      alert(JSON.stringify($scope.loginForm));
+      //$location.path('/info');
+    };
+
+    //Log in the user as a guest
+    $scope.loginAsGuest = function () {
+      //TODO: Mark user as a guest?
+      //$location.path('/info');
+    };
+
+    //Launch password recovery
+    $scope.recoverPassword = function () {
+      //TODO: Recover Password
+      alert("Your password is \'hunter2\'");
+    };
+
+    //Register a new user account
+    $scope.register = function () {
+      //TODO: Register the user
+      alert("COMING SOON!!!");
+    };
 
   });
