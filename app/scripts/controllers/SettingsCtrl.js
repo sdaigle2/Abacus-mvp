474819549557011dcf86156717314615ac925591
blackarctic <blackarctic@icloud.com>
2015-04-13 11:44:53 -0500

    Starting the Settings Page

---
+++ app/scripts/controllers/SettingsCtrl.js
@@ -0,0 +1,18 @@
+// jshint unused:false
+/* globals frameDataFromDB:true, cartDataFromDB:true, curWheelchair:true, $ */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abacuApp.controller:SettingsCtrl
+ * @description
+ * # SettingsCtrl
+ * Controller of the abacuApp
+ */
+angular.module('abacuApp')
+  .controller('SettingsCtrl', function ($scope, $location, sharedVars) {
+
+
+
+
+  });
