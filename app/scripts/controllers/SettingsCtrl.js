39ff4c513fcad2d0d60c7a61a62bb0c2f2e93bfd
Scott Blessing <scott.blessing@hotmail.com>
2015-04-21 15:51:50 -0500

    Partially Angularized 'Settings'

--- app/scripts/controllers/SettingsCtrl.js
+++ app/scripts/controllers/SettingsCtrl.js
@@ -12,14 +12,14 @@
 angular.module('abacuApp')
   .controller('SettingsCtrl', function ($scope, $location, sharedVars) {
 
-
-
+    //Sidebar options
     $scope.ContentSection = {
       ACCOUNT : 'account',
       ORDERS : 'orders',
       MEASUREMENTS : 'measurements'
     };
 
+    //Categories inside the 'My Measurements' Section
     $scope.MeasurementHelpSection = {
       NONE : '',
       REAR_SEAT_HEIGHT : 'rearSeatHeight',
@@ -29,40 +29,112 @@ angular.module('abacuApp')
       SEAT_DEPTH : 'seatDepth'
     };
 
+    //Navigational values
+    var contentSection = $scope.ContentSection.ACCOUNT;
+    var measurementHelpSection = $scope.MeasurementHelpSection.REAR_SEAT_HEIGHT;
 
 
-    $scope.contentSection = $scope.ContentSection.ACCOUNT;
-    $scope.measurementHelpSection = $scope.MeasurementHelpSection.REAR_SEAT_HEIGHT;
-
-
-
+    /***************** CONTENT SECTION SWITCHING *****************************/
     $scope.getContentSection = function () {
-      return $scope.contentSection;
+      return contentSection;
     };
 
-    $scope.setContentSection = function (contentSection) {
+    $scope.setContentSection = function (newContentSection) {
       $scope.resetMeasurementHelpSection();
-      $scope.contentSection = contentSection;
+      contentSection = newContentSection;
     };
 
     $scope.resetContentSection = function() {
-      $scope.contentSection = $scope.ContentSection.ACCOUNT;
+      contentSection = $scope.ContentSection.ACCOUNT;
     }
 
-
-
+    /***************** MEASUREMENT HELP SWITCHING *****************************/
     $scope.getMeasurementHelpSection = function () {
-      return $scope.measurementHelpSection;
+      return measurementHelpSection;
     };
 
-    $scope.setMeasurementHelpSection = function (measurementHelpSection) {
-      $scope.measurementHelpSection = measurementHelpSection;
+    $scope.setMeasurementHelpSection = function (newMeasurementHelpSection) {
+      measurementHelpSection = newMeasurementHelpSection;
     };
 
     $scope.resetMeasurementHelpSection = function () {
-      $scope.measurementHelpSection = $scope.MeasurementHelpSection.REAR_SEAT_HEIGHT;
+      measurementHelpSection = $scope.MeasurementHelpSection.REAR_SEAT_HEIGHT;
     }
 
+    /***************** MY ACCOUNT *********************************************/
 
+    //Model for the 'My Account' inputs
+    $scope.accountModel = {
+      fName: "",
+      lName: "",
+      email: "",
+      addr: "",
+      addr2: "",
+      city: "",
+      state: "",
+      zip: "",
+      newPass1: "",
+      newPass2: ""
+    };
+
+    /***************** MY ORDERS **********************************************/
+
+    //Array of orders
+    $scope.orders = [{
+      orderNum: "0000",
+      datePlaced: new Date(2015, 1, 15, 7, 30, 0, 0),
+      cart: {
+        //Various cart fields
+        totalPrice: 3721.90
+      }
+    },
+    {
+      orderNum: "0001",
+      datePlaced: new Date(2015, 1, 15, 8, 15, 0, 0),
+      cart: {
+        //Various cart fields
+        totalPrice: 9721.90
+      }
+    }];
+
+    $scope.openOrderDetails = function (index) {
+      //TODO: Display order details
+    };
+
+    /***************** MY MEASURES *********************************************/
+
+    //Options for each measure - Can be called using $scope.measOptions['rearSeatHeight'] to take advantage of enum
+    $scope.measOptions = {
+      rearSeatHeight: {
+        name: "Rear Seat Height",
+        options: ["1", "2", "3"],
+        desc: "Distance from the ground up to back corner of your seat",
+        imgURLs: []
+      },
+      rearSeatWidth: {
+        name: "Rear Seat Height",
+        options: ["1", "2", "3"],
+        desc: "Distance from the ground up to back corner of your seat",
+        imgURLs: []
+      },
+      foldingBackrestHeight: {
+        name: "Rear Seat Height",
+        options: ["1", "2", "3"],
+        desc: "Distance from the ground up to back corner of your seat",
+        imgURLs: []
+      },
+      axelPosition: {
+        name: "Rear Seat Height",
+        options: ["1", "2", "3"],
+        desc: "Distance from the ground up to back corner of your seat",
+        imgURLs: []
+      },
+      seatDepth: {
+        name: "Rear Seat Height",
+        options: ["1", "2", "3"],
+        desc: "Distance from the ground up to back corner of your seat",
+        imgURLs: []
+      }
+    };
 
   });
