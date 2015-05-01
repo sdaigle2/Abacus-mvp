668a52418dc22e7e2f02cc550748a72109953c66
Brian Zhou <brian@intelliwheels.net>
2015-05-01 12:33:27 -0500

    orderFactory

--- app/scripts/service/factory/orderFactory.js
+++ app/scripts/service/factory/orderFactory.js
@@ -10,9 +10,25 @@
  * Service of the abacuApp
  */
 angular.module('abacuApp')
-  .factory('Order', ['$http', function ($http){
+  .factory('Order', ['$http',function ($http, wheelchair){
 
+    var payMethod = "advance";
+    var subtotal = 0;
+    var tax = 0.097
+    var shipping = null;
+    var total = 0;
+    var orderWheelchairs = [];
 
+    function getTotal (){
+      if(orderWheelchairs.length > 0){
+        var total = 0;
+        for(var i = 0; i < orderWheelchairs.length; i++){
+          total += orderWheelchairs[i].getTotalPrice();
+        }
+        return total;
+      }
+      return 0;
+    }
 
     return{
       all: function(){
@@ -20,6 +36,35 @@ angular.module('abacuApp')
       },
       post: function(info){
         return $http({method:"POST", url:"data/orderData.json", data:info});
+      },
+      createOrder: function(payment, shippingFee, taxRate) {
+        payMethod = payment;
+        shipping = shippingMethod;
+        subtotal = getTotal();
+        tax = taxRate * subtotal;
+        total = shipping + subtotal + tax;
+        return true;
+      },
+      checkoutWheelchairs: function (curWheelchair){
+        orderWheelchairs.push(curWheelchair);
+      },
+      deleteWheelchair: function(index){
+        orderWheelchairs.splice(index,1);
+      },
+
+
+      //**************gets/sets************/
+      getPayMethod: function () {return payMethod},
+      getSubtotal: function () {return subtotal},
+      getTax: function () {return tax},
+      getShipping: function() {return shipping},
+      getTotal: function () {return total},
+      getOrderWheelchairs:function(){return orderWheelchair},
+
+      getWheelchair: function(index){
+        if(index >= 0 && index < orderWheelchairs.length)
+          return orderWheelchairs[index];
+        return null;
       }
     };
   }]);
