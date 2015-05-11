f502fe70641091441a037c4c3a187a1845f948de
Scott Blessing <scott.blessing@hotmail.com>
2015-05-11 12:00:56 -0500

    Fixed minor about page issue

--- app/scripts/controllers/AboutCtrl.js
+++ app/scripts/controllers/AboutCtrl.js
@@ -11,31 +11,40 @@ angular.module('abacuApp')
   .controller('AboutCtrl', ['$scope', function ($scope) {
 
     $scope.employees = [{
-      name: 'Marissa Siebel'
+      name: 'Marissa Siebel',
+      extra: 'Lorem ipsum'
     },
     {
-      name: 'Scott Daigle'
+      name: 'Scott Daigle',
+      extra: 'dummy text goes here'
     },
     {
-      name: 'Josh George'
+      name: 'Josh George',
+      extra: 'Actually a World Champion'
     },
     {
-      name: 'Brian Zhou'
+      name: 'Brian Zhou',
+      extra: 'Clash of clans World Champion 2015'
     },
     {
-      name: 'Steven Pick'
+      name: 'Steven Pick',
+      extra: 'dummy text goes here'
     },
     {
-      name: 'Matt Kowalec'
+      name: 'Matt Kowalec',
+      extra: 'dummy text goes here'
     },
     {
-      name: 'Sarah Cho'
+      name: 'Sarah Cho',
+      extra: 'Didn\'t have any say on this page\'s design'
     },
     {
-      name: 'Scott Blessing'
+      name: 'Scott Blessing',
+      extra: 'He\'s cool'
     },
     {
-      name: 'Nick Galloway'
+      name: 'Nick Galloway',
+      extra: 'He\'s also cool'
     },
     ]
 
