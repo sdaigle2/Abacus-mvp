15aaa4727fc9c7444425b3242f2846011a75bee6
DhruvVajpeyi <vajpeyi2@gmail.com>
2015-05-27 18:08:37 -0500

    Javascript on invoice page started. First page complete

--- app/scripts/controllers/checkout/InvoiceCtrl.js
+++ app/scripts/controllers/checkout/InvoiceCtrl.js
@@ -10,12 +10,38 @@
  * Controller of the abacuApp
  */
 angular.module('abacuApp')
-  .controller('InvoiceCtrl', ['$scope', '$location', 'User', function ($scope, $location, User) {
-    var order = User.getCurEditOrder();
+  .controller('InvoiceCtrl', ['$scope', '$location', 'User', 'FrameData', function ($scope, $location, User, FrameData) {
+    var order = User.getLastOrder();
     $scope.name = User.getFullName();
     $scope.wheelchairs = order.getWheelchairs();
     $scope.orderNum = order.getOrderNum();
 
+    function getFrame(wheelchair){
+      var frameID = wheelchair.getFrameID();
+      return FrameData.getFrame(frameID);
+    }
 
+    $scope.getManufacturer = function(wheelchair){
+      var frame = getFrame(wheelchair);
+      return frame.getManufacturer();
+    };
 
+    $scope.getBasePrice = function(wheelchair){
+      var frame = getFrame(wheelchair);
+      return '$'+frame.getBasePrice();
+    };
+
+    $scope.getModel = function(wheelchair){
+      var frame = getFrame(wheelchair);
+      return frame.getName();
+    };
+
+    $scope.getBaseWeight = function(wheelchair){
+      var frame = getFrame(wheelchair);
+      return frame.getBaseWeight()+'lb';
+    };
+
+    $scope.getPreviewImages = function(wheelchair, angle){
+      return wheelchair.getPreviewImages(angle);
+    };
   }]);
