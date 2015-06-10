360f51403f03fc1e2abe378262f6281bf63c02d3
DhruvVajpeyi <vajpeyi2@gmail.com>
2015-06-10 17:07:29 -0500

    Small change to server. Changes base path to be compatible with angular code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/app'));
 app.get('*', function(req, res) {
   res.sendfile('./app/index.html');
 });
