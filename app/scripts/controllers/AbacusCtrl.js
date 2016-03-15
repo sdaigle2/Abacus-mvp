cc6a483156c93bdd222f64671d9680af87347458
Brian Zhou <brian@intelliwheels.net>
2016-03-15 17:21:48 -0500

    tinker checkout action panel group done  //TODO: cosmetic side

--- app/scripts/controllers/AbacusCtrl.js
+++ app/scripts/controllers/AbacusCtrl.js
@@ -212,6 +212,19 @@ angular.module('abacuApp')
         $location.path('/settings');
       };
 
+      //redirect
+      $scope.backToMain = function(){
+        $scope.loginPanel = loginPanelStatus.MAIN;
+      };
+
+      $scope.backToLogin = function(){
+        $scope.loginPanel = loginPanelStatus.LOGIN;
+      };
+
+      $scope.backToMain = function(){
+        $scope.loginPanel = loginPanelStatus.MAIN;
+      };
+
       /****************Weight and Price******************/
 
       $scope.getTotalWeight = function () {
@@ -490,14 +503,9 @@ angular.module('abacuApp')
       }
 
       /*********Save $ review Dropdown*********/
-      $scope.toggleSaveDropDown = function () {
 
-        if(1){
-          $scope.saveDropdown = !$scope.saveDropdown;
-        }
-      };
 
-      $scope.openSaveDropDown = function() {
+      $scope.saveDropDown = function() {
         $scope.saveDropdown = true;
         console.log('im opening')
       };
@@ -791,7 +799,6 @@ angular.module('abacuApp')
             .then(function (design) {
               if (design instanceof Design) {
                 User.addDesignIDToSavedChairs(design.id);
-                $location.path('/mydesigns');
               }
             })
             .catch(function (design) {
