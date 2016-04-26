bc3fd1c2ce91c7b76c9283864ced42ec5459d76b
sourabhdesai <sourabhdesai@gmail.com>
2016-04-26 15:20:13 -0500

    add helper functions for testing

---
+++ tests/backend_tests/helpers/createNewUser.js
@@ -0,0 +1,31 @@
+/**
+ * Created by sourabhdesai on 4/26/16.
+ *
+ * Creates a new user with randomly populated attributes
+ */
+
+const Chance  = require('chance');
+const _       = require('lodash');
+
+const chance = new Chance();
+
+module.exports = () => {
+  var password = chance.string({length: 5});
+  return {
+    fName: chance.first(),
+    lName: chance.last(),
+    email: chance.email(),
+    phone: chance.phone(),
+    addr: chance.address(),
+    addr2: '',
+    city: chance.city(),
+    state: chance.state(),
+    zip: chance.zip(),
+    password: password,
+    confirm: password, //Second typing of password
+    unitSys: 0,
+    orders: [],
+    savedDesigns: [],
+    cart: null
+  };
+};
