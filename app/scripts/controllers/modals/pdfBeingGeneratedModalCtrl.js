ebc7a22db1ac1303f1b411dbda5a58672a971c4f
sdaigle2 <scott@intelliwheels.net>
2016-08-24 15:24:51 -0500

    Added New Modal Files for PDF BEING GENERATED
    Added the new files to create the modal that indicates that the PDF is
    being generated.

---
+++ app/scripts/controllers/modals/pdfBeingGeneratedModalCtrl.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abacuApp.controller:DesignIDModalCtrl
+ * @description
+ * # DesignIDModalCtrl
+ * Controller of the abacuApp
+ */
+angular.module('abacuApp')
+	.controller('pdfBeingGeneratedModalCtrl', ['$scope', '$location',
+	function ($scope, $location) {
+		/*
+    $scope.copied = false;
+
+		$scope.onCopySuccess = function (e) {
+			$scope.copied = true;
+		};
+
+		$scope.onCopyFailure = function (e) {
+			$scope.copied = false;
+		};
+
+		$scope.getTinkerLink = function (designID) {
+			var baseURL = _.trimEnd($location.absUrl(), $location.path());
+			return baseURL + '/tinker/' + designID;
+		};
+*/
+	}]);
