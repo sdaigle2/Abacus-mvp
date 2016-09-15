2329cbe71af9d1ed0b7334c4763d9351da4d86a5
1 <a1@MacBook-Air-1.local>
2016-09-15 11:45:41 +0300

    minor style changes

--- app/scripts/services/services/payments/paymentService.js
+++ app/scripts/services/services/payments/paymentService.js
@@ -1,40 +1,41 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('abacuApp')
-  .service('PaymentAPI', ['$http', function ($http) {
+  angular
+    .module('abacuApp')
+    .service('PaymentAPI', payment);
+    
+    payment.$inject = ['$http'];
 
-    function createPayment(amount, payType, token, order, creditCard, checkNum, memo) {
-      return $http({
-          url: 'orders/create-payment',
-          method: 'POST',
-          data: {
-            paymentAmount: amount,
-            payType: payType,
-            token: token, 
-            order: order, 
-            creditCard: creditCard, 
-            checkNum: checkNum,
-            memo: memo
-          }
-      })
-      .then(pushResponse)
-      .catch(handleError)
-    };
+    function payment($http) {
+      this.createPayment = function(amount, payType, token, order, creditCard, checkNum, memo) {
+        return $http({
+            url: 'orders/create-payment',
+            method: 'POST',
+            data: {
+              paymentAmount: amount,
+              payType: payType,
+              token: token, 
+              order: order, 
+              creditCard: creditCard, 
+              checkNum: checkNum,
+              memo: memo
+            }
+        })
+        .then(pushResponse)
+        .catch(handleError)
+      };
 
-    function pushResponse(resp) {
-        return resp.data;
-    };
+      function pushResponse(resp) {
+          return resp.data;
+      };
 
-    function handleError(err) {
-        if (err instanceof Error) {
-            throw err;
-        } else {
-            throw new Error(err.data.msg || err.msg);
-        } 
-    };
-
-    return {
-      createPayment: createPayment
-    };
-  }]);
-  
+      function handleError(err) {
+          if (err instanceof Error) {
+              throw err;
+          } else {
+              throw new Error(err.data.msg || err.msg);
+          } 
+      };
+    }
+})();
