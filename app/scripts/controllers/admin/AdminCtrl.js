de92f7ccd19388d21f4d47b4dab061ba9bf0b42b
1 <a1@MacBook-Air-1.local>
2016-09-19 18:20:57 +0300

    limits discounts for non superAdmin users

--- app/scripts/controllers/admin/AdminCtrl.js
+++ app/scripts/controllers/admin/AdminCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module('abacuApp')
   .controller('AdminCtrl', ['$scope', '$location', 'User', '_', 'AdminAPI',  function ($scope, $location, User, _, AdminAPI) {
-
+  	var adminLimitMessage = 'As an admin, you may only create discount codes no greater than 25%. Please contact Chris, Danny, or Cesar for approval to create a larger discount.';
   	function validateFields(obj) {
   		if (!obj.startDate || !obj.endDate || !obj.id || !obj.percent) return 'Please fill in all the fields.'
 		if (typeof obj.id !== 'string') return 'ID should be letters and numbers.'
@@ -44,6 +44,11 @@ angular.module('abacuApp')
 			$scope.discount.percent = $scope.discount.percent / 100;
 			$scope.discount.id = $scope.discount.id.toLowerCase();
 			$scope.errorMsg = '';
+			
+			if (User.getUserType() !== 'superAdmin' && $scope.discount.percent > 0.25) {
+				$scope.discount = {};
+				return $scope.errorMsg = adminLimitMessage;
+			}
 			
 			AdminAPI.createDiscount($scope.discount)
 			.then(function() {
