dd41a4cd56672756ae3dbfcba8fe77ef16142075
Hamza Muazzam Iqbal <hamzammuazzam172@gmail.com>
2023-06-20 21:17:35 +0500

    Update index.js

--- server_scripts/services/generatePDF/htmlToPDF/index.js
+++ server_scripts/services/generatePDF/htmlToPDF/index.js
@@ -12,8 +12,11 @@ async function htmlToPDF(args, cb) {
 
 	try {
 		const browser = await puppeteer.launch({
-			headless: 'new'
-		});
+			'args' : [
+			  '--no-sandbox',
+			  '--disable-setuid-sandbox'
+			]
+		  });
 		const page = await browser.newPage();
 		await page.setContent(rawHTML);
 
