00c9de3e378575d64a007ff39fd3a4ed1f0b652b
Hamza Muazzam Iqbal <hamzammuazzam172@gmail.com>
2023-06-22 20:57:08 +0500

    pdf library

--- server_scripts/services/generatePDF/htmlToPDF/index.js
+++ server_scripts/services/generatePDF/htmlToPDF/index.js
@@ -1,27 +1,19 @@
+const pdf = require('html-pdf');
 const fs = require('fs');
-const htmlToPdf = require('html-pdf-node');
 
-async function htmlToPDF(args, cb) {
+function htmlToPDF(args, cb) {
   const pdfFilePath = args.pdfFilePath;
   const rawHTML = args.rawHTML;
 
-  try {
-    const file = { content: rawHTML };
-    const options = { format: 'A4' };
-
-    const pdfBuffer = await htmlToPdf.generatePdf(file, options);
-    fs.writeFile(pdfFilePath, pdfBuffer, (err) => {
-      if (err) {
-        console.error('PDF write file error:', err);
-        cb(err);
-      } else {
-        cb(null, pdfFilePath);
-      }
-    });
-  } catch (err) {
-    console.error('HTML to PDF conversion error:', err);
-    cb(err);
-  }
+  pdf.create(rawHTML).toFile(pdfFilePath, function(err, res) {
+    if (err) {
+      console.error('PDF creation error:', err);
+      cb(err);
+    } else {
+      console.log('PDF file created:', res.filename);
+      cb(null, res.filename);
+    }
+  });
 }
 
 module.exports = htmlToPDF;
