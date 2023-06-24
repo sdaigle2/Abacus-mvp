8faa3c626225d43046b2dac95114103e665c3447
Hamza Muazzam Iqbal <hamzammuazzam172@gmail.com>
2023-06-24 05:06:55 +0500

    Invoice fix

--- server_scripts/services/generatePDF/htmlToPDF/index.js
+++ server_scripts/services/generatePDF/htmlToPDF/index.js
@@ -1,12 +1,14 @@
 const PCR = require('puppeteer-chromium-resolver');
 const fs = require('fs');
+const { Readable } = require('stream');
+
 
 async function htmlToPDF(args, cb) {
   const pdfFilePath = args.pdfFilePath;
   const rawHTML = args.rawHTML;
 
-  try {
-    const options = {timeout: 60000,};
+  try { 
+    const options = { timeout: 60000 };
     const stats = await PCR(options);
     const browser = await stats.puppeteer.launch({
       // headless: false,
@@ -21,15 +23,23 @@ async function htmlToPDF(args, cb) {
 
     await browser.close();
 
-    fs.writeFile(pdfFilePath, pdfBuffer, (err) => {
-      if (err) {
-        console.error('PDF write file error:', err);
-        cb(err);
-      } else {
-        console.log('PDF file created:', pdfFilePath);
-        cb(null, pdfFilePath);
-      }
-    });
+    if (!pdfFilePath) {
+      // If no file path to save the PDF was given, return the raw stream for the PDF
+      const stream = new Readable();
+      stream.push(pdfBuffer);
+      stream.push(null);
+      cb(null, stream);
+    } else {
+      fs.writeFile(pdfFilePath, pdfBuffer, (err) => {
+        if (err) {
+          console.error('PDF write file error:', err);
+          cb(err);
+        } else {
+          console.log('PDF file created:', pdfFilePath);
+          cb(null, pdfFilePath);
+        }
+      });
+    }
   } catch (err) {
     console.error('HTML to PDF conversion error:', err);
     cb(err);
