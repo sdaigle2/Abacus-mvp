c308b58ad5d628dcdfaedad6ddade91d79055275
Hamza Muazzam Iqbal <hamzammuazzam172@gmail.com>
2023-06-24 05:16:45 +0500

    Update index.js

--- server_scripts/services/generatePDF/htmlToPDF/index.js
+++ server_scripts/services/generatePDF/htmlToPDF/index.js
@@ -8,7 +8,7 @@ async function htmlToPDF(args, cb) {
   const rawHTML = args.rawHTML;
 
   try { 
-    const options = { timeout: 90000 };
+    const options = { timeout: 180000 };
     const stats = await PCR(options);
     const browser = await stats.puppeteer.launch({
       // headless: false,
